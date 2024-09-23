f952c79c125b999d020342c55a63d716b6b62a96
mines213 <128561293+mines213@users.noreply.github.com>
2024-09-23 21:11:45 +0900

    [Platinum IV] Title: 도미노, Time: 116 ms, Memory: 30784 KB -BaekjoonHub

---
+++ 백준/Platinum/4196. 도미노/도미노.cc
@@ -0,0 +1,97 @@
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+#define ll long long
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
+using namespace std;
+#define MAX 100001
+#define X first
+#define Y second
+
+int id;
+int SN; //scc의 개수
+int sn[MAX]; //sn[i] = i가 속한 SCC의 번호
+int d[MAX];
+bool finished[MAX];
+vector<int> graph[MAX];
+vector<vector<int> > SCC;
+stack<int> s;
+
+int dfs(int x) {
+  d[x] = ++id;
+  s.push(x);
+  int parent = d[x];
+  for (int nxt: graph[x]) {
+    if (d[nxt] == 0) parent = min(parent, dfs(nxt));
+    else if (!finished[nxt]) parent = min(parent, d[nxt]);
+  }
+  if (parent == d[x]) {
+    vector<int> scc;
+    while (1) {
+      int t = s.top();
+      s.pop();
+      scc.push_back(t);
+      finished[t] = true;
+      sn[t] = SN;
+      if (t == x) break;
+    }
+    SCC.push_back(scc);
+    SN++;
+  }
+  return parent;
+}
+
+void solve() {
+  id = 0;
+  SN = 0;
+  memset(d, 0, sizeof(d));
+  memset(finished, 0, sizeof(finished));
+  for (int i = 0; i < MAX; i++) {
+    graph[i].clear();
+  }
+  SCC.clear();
+  int v, e;
+  cin >> v >> e;
+  for (int i = 0; i < e; i++) {
+    int a, b;
+    cin >> a >> b;
+    graph[a].push_back(b);
+  }
+  for (int i = 1; i <= v; i++) {
+    if (d[i] == 0) dfs(i);
+  }
+  int SCC_indgree[SN] = {0};
+  for (int i = 1; i <= v; i++) {
+    for (int it: graph[i]) {
+      if (sn[i] != sn[it]) SCC_indgree[sn[it]]++;
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < SN; i++) {
+    if (SCC_indgree[i] == 0) ans++;
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  fastio;
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
