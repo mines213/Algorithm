58a7e3967b9258b7b36dfc409671ad6f1a23582d
mines213 <128561293+mines213@users.noreply.github.com>
2024-10-09 04:14:23 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define X first
+#define Y second
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
+using namespace std;
+
+
+int main() {
+    fastio;
+    long double N;
+    cin >> N;
+    cout << fixed;
+    cout.precision(2);
+    cout << N / 4;
+    return 0;
+}
