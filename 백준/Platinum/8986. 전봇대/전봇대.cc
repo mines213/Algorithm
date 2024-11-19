1143ab1d88f27fce27cd01ee7ba210377eca64be
mines213 <128561293+mines213@users.noreply.github.com>
2024-11-19 19:21:36 +0900

    [Platinum V] Title: 전봇대, Time: 20 ms, Memory: 2412 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/8986. 전봇대/전봇대.cc
+++ 백준/Platinum/8986. 전봇대/전봇대.cc
@@ -1,4 +1,3 @@
-//kks227 블로그에서 공부 후 작성
 #include <bits/stdc++.h>
 #define ll long long
 #define X first
@@ -8,7 +7,7 @@ using namespace std;
 int N;
 int arr[100005];
 
-ll f(int distance) {
+ll f(ll distance) {
     ll result = 0;
     for (int i = 1; i < N; i++)
         result += abs(1ll * distance * i - arr[i]);
@@ -22,14 +21,14 @@ int main() {
         cin >> arr[i];
     }
 
-    int lo = 0, hi = arr[N - 1];
+    ll lo = 0, hi = arr[N - 1];
     while (hi - lo >= 3) {
-        int p = (lo * 2 + hi) / 3, q = (lo + hi * 2) / 3;
+        ll p = (lo * 2 + hi) / 3, q = (lo + hi * 2) / 3;
         if (f(p) <= f(q)) hi = q;
         else lo = p;
     }
 
-    ll res = 1e17;
+    ll res = 1e18;
     for (int i = lo; i <= hi; i++) {
         res = min(f(i), res);
     }
